c8af279e05b3f3eca8d2497f2d9af8a9845e2ecd
Ashwin Nair <ashwin95.nair@gmail.com>
2021-05-01 16:52:08 +0530

    renamed package and modified code for plain text

---
+++ restlet/build/doctor_patient/PlainResourceDoctor.java
@@ -0,0 +1,28 @@
+package doctor_patient;
+
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.data.Status;
+import org.restlet.data.MediaType;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class PlainResourceDoctor extends ServerResource {
+CopyOnWriteArrayList<Doctor> doctors;
+
+    public PlainResourceDoctor() { }
+    @Get
+    public Representation toPlain() {
+        String output="";
+      doctors = Doctors.getList();
+	for (Doctor a : doctors) {
+        output=output+a.getId()+" : "+a.getName()+"==> Patient Count : "+a.getPatientCount()+"\n";
+	}	
+    
+	setStatus(Status.SUCCESS_OK);
+	return new StringRepresentation(output, MediaType.TEXT_PLAIN);
+    }
+
+   
+}
---
+++ restlet/src/doctor_patient/XmlAllResourcePatient.java
@@ -0,0 +1,43 @@
+package doctor_patient;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+import org.restlet.representation.Representation;
+import org.restlet.ext.xml.DomRepresentation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.restlet.data.Status;
+import org.restlet.data.MediaType;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.*;
+import java.io.IOException;
+
+public class XmlAllResourcePatient extends ServerResource {
+	private static Logger logger = Logger.getLogger(XmlAllResource.class.getName());
+
+	public XmlAllResourcePatient() {
+	}
+
+
+	@Get
+	public Representation toXml() throws Exception {
+
+		CopyOnWriteArrayList<Patient> patients;
+		patients = Patients.getList();
+
+		DomRepresentation dom = null;
+		try {
+			dom = new DomRepresentation(MediaType.TEXT_XML);
+			dom.setIndenting(true);
+			Document doc = dom.getDocument();
+			Element root = DoctorPatientUtil.getPatientsXml(null,doc);
+			// root.appendChild(root1);
+			doc.appendChild(root);
+		} catch (Exception e) {
+			throw e;
+		}
+		return dom;
+	}
+
+
+}
